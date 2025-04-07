1626eb2ea64f8d422493c4f8141b19f507d60908
Your Name <koom@c1>
2025-04-08 00:42:05 +0200

    test

--- plugins/window-state/src/lib.rs
+++ plugins/window-state/src/lib.rs
@@ -230,8 +230,8 @@ impl<R: Runtime> WindowExt for Window<R> {
             {
                 let ss = m.size();
                 {
-                    let w = ss.width * 90 / 100;
-                    let h = ss.height * 90 / 100;
+                    let w = ss.width * 99 / 100;
+                    let h = ss.height * 99 / 100;
 
                     self.set_size(PhysicalSize {
                         width: w as u32,
