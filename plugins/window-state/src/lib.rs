c97a174b52aadd8578712fb31203204675ecdf15
Your Name <koom@c1>
2025-04-08 00:35:31 +0200

    test

--- plugins/window-state/src/lib.rs
+++ plugins/window-state/src/lib.rs
@@ -226,13 +226,16 @@ impl<R: Runtime> WindowExt for Window<R> {
         } else {
 
             let s = self.current_monitor()?.size();
-            let w = s.width * 0.8;
-            let h = s.height * 0.8;
+            if let Some(ss) = s
+            {
+                let w = ss.width * 0.8;
+                let h = ss.height * 0.8;
 
-            self.set_size(PhysicalSize {
-                width: w as u32,
-                height: h as u32,
-            })?;
+                self.set_size(PhysicalSize {
+                    width: w as u32,
+                    height: h as u32,
+                })?;
+            }
 
 
             let mut metadata = WindowState::default();
