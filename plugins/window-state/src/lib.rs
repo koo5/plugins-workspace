d7c6f01c89a3f65350a37f462a630b0bd4797ff8
Your Name <koom@c1>
2025-04-08 00:40:06 +0200

    test

--- plugins/window-state/src/lib.rs
+++ plugins/window-state/src/lib.rs
@@ -228,7 +228,7 @@ impl<R: Runtime> WindowExt for Window<R> {
 
             if let Some(Some(m)) = self.current_monitor().ok()
             {
-                if let Some(ss) = m.size()
+                let ss = m.size();
                 {
                     let w = ss.width * 0.8;
                     let h = ss.height * 0.8;
